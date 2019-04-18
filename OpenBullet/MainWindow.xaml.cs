fa32ebe91fe489a069690508d4a2423823ec5bdc
ForlaxPy <47989005+ForlaxPy@users.noreply.github.com>
2019-04-18 20:35:39 +0000

    [HELP] windows options like other software

--- OpenBullet/MainWindow.xaml.cs
+++ OpenBullet/MainWindow.xaml.cs
@@ -353,6 +353,32 @@ namespace OpenBullet
             }
         }
         
+        private void maximizePanel_MouseDC(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+
+                if (maximized)
+                {
+                    this.Width = 800;
+                    this.Height = 620;
+                    Left = 0;
+                    Top = 0;
+                    maximized = false;
+                    WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    this.Width = SystemParameters.WorkArea.Width;
+                    this.Height = SystemParameters.WorkArea.Height;
+                    Left = 0;
+                    Top = 0;
+                    maximized = true;
+                    WindowState = WindowState.Normal;
+                }
+            }
+        }
+        
         private void minimizePanel_MouseEnter(object sender, MouseEventArgs e)
         {
             minimizePanel.Background = new SolidColorBrush(Colors.DimGray);
