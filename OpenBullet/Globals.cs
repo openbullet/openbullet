90a3627d9a12e368687d61277e9a5990a3e66823
ruri <ruridev@protonmail.com>
2019-04-25 01:10:17 +0200

    Bumped version to 1.1.0.

--- OpenBullet/Globals.cs
+++ OpenBullet/Globals.cs
@@ -32,7 +32,7 @@ namespace OpenBullet
     public static class Globals
     {
         // Version
-        public static string obVersion = "1.0.2";
+        public static string obVersion = "1.1.0";
 
         // Main Window
         public static MainWindow mainWindow;
