1979d6ff12238f538e87c0841b57b79b401beb59
ruri <ruridev@protonmail.com>
2019-12-20 01:40:38 +0100

    Added Repository implementations

---
+++ OpenBullet/Repositories/ConfigRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RuriLib;
+using RuriLib.Interfaces;
+using RuriLib.ViewModels;
+
+namespace OpenBullet.Repositories
+{
+    public class ConfigRepository : IRepository<ConfigViewModel, string>
+    {
+        public string BaseFolder { get; set; }
+
+        public ConfigRepository(string baseFolder)
+        {
+            BaseFolder = baseFolder;
+        }
+
+        public void Add(ConfigViewModel entity)
+        {
+            if (!File.Exists(entity.Path))
+            {
+                IOManager.SaveConfig(entity.Config, entity.Path);
+            }
+            else
+            {
+                throw new IOException("A file with the same name already exists");
+            }
+        }
+
+        public IEnumerable<ConfigViewModel> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ConfigViewModel Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(ConfigViewModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ConfigViewModel entity)
+        {
+            if (File.Exists(entity.Path))
+            {
+                IOManager.SaveConfig(entity.Config, entity.Path);
+            }
+            else
+            {
+                throw new IOException("File not found");
+            }
+        }
+    }
+}
---
+++ OpenBullet/Repositories/LiteDBRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+using RuriLib.Interfaces;
+using RuriLib.Models;
+
+namespace OpenBullet.Repositories
+{
+    public class LiteDBRepository<T> : IRepository<T, Guid> where T : Persistable<Guid>
+    {
+        private LiteDatabase _db;
+        private LiteCollection<T> _coll;
+
+        public string Name { get; set; }
+
+        public LiteDBRepository(string name)
+        {
+            Name = name;
+            _db = new LiteDatabase(Globals.dataBaseFile);
+            _coll = _db.GetCollection<T>(name);
+        }
+
+        ~LiteDBRepository()
+        {
+            _db.Dispose();
+        }
+
+        public void Add(T entity)
+        {
+            _coll.Insert(entity);
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _coll.FindAll();
+        }
+
+        public T Get(Guid id)
+        {
+            return _coll.FindById(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _coll.Delete(entity.Id);
+        }
+
+        public void RemoveAll()
+        {
+            _db.DropCollection(Name);
+        }
+
+        public void Update(T entity)
+        {
+            _coll.Update(entity);
+        }
+    }
+}
